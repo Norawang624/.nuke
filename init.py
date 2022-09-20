c13837e747e5b6f1b530e430998b06192eb47938
Norawang624 <113998270+Norawang624@users.noreply.github.com>
2022-09-20 06:51:13 -0700

    Update init.py
    Initial.init.py

--- init.py
+++ init.py
@@ -1,1 +1,21 @@
+#	------------------------
+#	init.py
+#	Version: 1.0.0
+#	Last Updated: Sep-20-2022
+#	------------------------
+#	------Define-Custom-Folder-Structure-----
 
+
+import nuke
+
+nuke.pluginAddPath('pixelfudger')
+nuke.pluginAddPath('Gizmo')
+nuke.pluginAddPath('icon')
+nuke.pluginAddPath('python')
+nuke.pluginAddPath('./higx/PointRender')
+nuke.pluginAddPath('./NukeSurvivalToolkit')
+
+import cryptomatte_utilities
+cryptomatte_utilities.setup_cryptomatte()
+
+nuke.pluginAddPath('./higx/PointRender')
