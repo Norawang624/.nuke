ce6ba2b3e7a286ebcd144b77f3b5efa6ac5e860c
Norawang624 <113998270+Norawang624@users.noreply.github.com>
2022-09-21 08:13:01 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,19 +1,20 @@
 #	------------------------
 #	menu.py
 #	Version: 1.0.0
-#	Last Updated: Sep-20-2022
+#	Last Updated: Sep-21-2022
 #	------------------------
 
-
+#---------------------------------------------------------------------
+#Global Imports::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------------------
 
 import sys
 import nuke
-
+import nukescripts
 import pixelfudger
 nuke.knobDefault('Write.channels','rgba')
 import cryptomatte_utilities
 cryptomatte_utilities.setup_cryptomatte_ui()
-
 import platform
 
 # Define where .nuke directory is on each OS's network.
@@ -31,8 +32,38 @@ elif platform.system() == "Linux":
 else:
 	dir = None
 
+
+#---------------------------------------------------------------------
+#Knob Defaults::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------------------
+
+nuke.knobDefault('Tracker4.shutteroffset', "centered")
+nuke.knobDefault('Tracker4.label', "Motion: [value transform]\nRef frame: [value reference_frame]")
+nuke.addOnUserCreate(lambda:nuke.thisNode()['reference_frame'].setValue(nuke.frame()), nodeClass='Tracker4')
+
+
+
+
+#---------------------------------------------------------------------
+#Custom Menus::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------------------
+
+utilitiesMenu = nuke.menu('Nuke').addMenu('Utilities')
+utilitiesMenu.addCommand('Autocrop', 'nukescripts.autocrop()')
+
+
+#--------------------------------------------------------------------------
+#KeyBoard Shortcuts::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#--------------------------------------------------------------------------
+
+nuke.menu('Nodes').addCommand("Transform/Tracker", "nuke.createNode('Tracker4')", "ctrl+alt+t", icon="Tracker.png", shortcutContext=2)
+
+
+#---------------------------------------------------------------------
+#Knob Gizmos::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#---------------------------------------------------------------------
 toolbar = nuke.menu('Nodes')
-NW = toolbar.addMenu('CNW', icon='V_Victor.png')
+NW = toolbar.addMenu('CNW', icon= dir+"/icons/myGizmos_icon.png")
 
 NW.addCommand("Filter/VectorExtendEdge", 'nuke.createNode("VectorExtendEdge")')
 NW.addCommand("Filter/EdgeExtend", 'nuke.createNode("EdgeExtend")')
@@ -41,7 +72,7 @@ NW.addCommand("Filter/KillOutline", 'nuke.createNode("KillOutline")')
 NW.addCommand("Filter/Mix_Fine_Detail_F", 'nuke.createNode("Mix_Fine_Detail_F")')
 NW.addCommand("Filter/Edge_RimLight", 'nuke.createNode("Edge_RimLight")')
 NW.addCommand("Filter/Fractal_Blur", 'nuke.createNode("Fractal_Blur")')
-
+NW.addCommand("Filter/EdgeMask_N", 'nuke.createNode("EdgeMask_N")', "n")
 
 NW.addCommand("Defocus/OpticalZDefocus", 'nuke.createNode("OpticalZDefocus")')
 
@@ -61,4 +92,3 @@ NW.addCommand("CC/ColorSampler", 'nuke.createNode("ColorSampler")')
 NW.addCommand("FX/P_Blood_Hit_v5", 'nuke.createNode("P_Blood_Hit_v5")')
 
 NW.addCommand("Key/Filler_Sampler", 'nuke.createNode("Filler_Sampler")')
-
